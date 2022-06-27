6a3cc4d0f19cfa1c93933272bc3b6ed8ea6d0cd5
Reese Rifredi <reese.rifredi@gmail.com>
2022-06-27 16:46:06 -0700

    pages

---
+++ pages/contactme.js
@@ -0,0 +1,8 @@
+console.log ("Hello World")
+
+$(document).ready(function (){
+    $('.submit').click(function (event) {
+    event.preventDefault()
+    console.log("button was clicked")
+    })
+})
